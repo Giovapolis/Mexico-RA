4c25d04de53b71ed23244cdefeb76fa57f616da5
Giovapolis <giovapolis@gmail.com>
2019-10-27 09:25:34 -0600

    Actualizacion de repositorio
    Prototipo de Proyecto

---
+++ Assets/Scripts/Boton_Next.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boton_Next : MonoBehaviour
+{
+    private string nombreUsuario;
+
+    public InputField nombreUsuarioField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void registrar() {
+        
+    }
+}
---
+++ Assets/Scripts/ControlAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlAudio : MonoBehaviour
+{
+    public GameObject clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer mesh = this.GetComponent<MeshRenderer>();
+        if (mesh.enabled){
+            clip.GetComponent<AudioSource>().Play();
+        }
+    }
+
+}
